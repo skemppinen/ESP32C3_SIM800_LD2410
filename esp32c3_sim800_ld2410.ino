65201c3537af06d0b6ab8b800836116c82ad8451
skemppinen <82161700+skemppinen@users.noreply.github.com>
2025-01-08 13:08:57 +0200

    Update esp32c3_sim800_ld2410.ino

--- esp32c3_sim800_ld2410.ino
+++ esp32c3_sim800_ld2410.ino
@@ -1,19 +1,35 @@
-// Select your modem (SSL/TLS is currently supported only with SIM8xx series) //
+//Select your modem
+//SSL/TLS is currently supported only with SIM8xx series
 #define TINY_GSM_MODEM_SIM800
 
-// Increase RX buffer //
+//Increase RX buffer
 #define TINY_GSM_RX_BUFFER 256
 
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 #include <TinyGsmClient.h> //https://github.com/vshymanskyy/TinyGSM
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
-// Your GPRS credentials. Leave empty, if missing user or pass //
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+// Your GPRS credentials
+// Leave empty, if missing user or pass
 char apn[]  = "prepaid.dna.fi";
 char user[] = "";
 char pass[] = "";
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
-//GSM Module RX & TX PIN //
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//GSM Module RX pin to ESP32 2
+//GSM Module TX pin to ESP32 4
 #define rxPin 0
 #define txPin 1
+HardwareSerial sim800(1);
+TinyGsm modem(sim800);
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+TinyGsmClientSecure gsm_client_secure_modem(modem, 0);
+//NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
 
 //LD2410 module RX & TX PIN //
 //#define MONITOR_SERIAL Serial
@@ -24,11 +40,6 @@ char pass[] = "";
 ld2410 radar;
 uint32_t lastReading = 0;
 bool radarConnected = false;
-
-HardwareSerial RADAR_SERIAL(2);
-HardwareSerial sim800(1);
-TinyGsm modem(sim800);
-TinyGsmClientSecure gsm_client_secure_modem(modem, 0);
 
 void loop(){
 // SIM //
@@ -54,27 +65,27 @@ void loop(){
  {
  if(radar.stationaryTargetDetected())
  {
- Serial.print(F("Stationary target: "));
- Serial.print(radar.stationaryTargetDistance());
- Serial.print(F("cm energy:"));
- Serial.print(radar.stationaryTargetEnergy());
- Serial.print(' ');
+ sim800.print(F("Stationary target: "));
+ sim800.print(radar.stationaryTargetDistance());
+ sim800.print(F("cm energy:"));
+ sim800.print(radar.stationaryTargetEnergy());
+ sim800.print(' ');
  }
  if(radar.movingTargetDetected())
  {
- Serial.print(F("Moving target: "));
- Serial.print(radar.movingTargetDistance());
- Serial.print(F("cm energy:"));
- Serial.print(radar.movingTargetEnergy());
+ sim800.print(F("Moving target: "));
+ sim800.print(radar.movingTargetDistance());
+ sim800.print(F("cm energy:"));
+ sim800.print(radar.movingTargetEnergy());
  }
- Serial.println();
+ sim800.println();
  }
  else
  {
- Serial.println(F("No target"));
+ sim800.println(F("No target"));
  }
  } else {
-  Serial.println(F("No target"));
+  sim800.println(F("No target"));
  }
 
 }
@@ -95,14 +106,14 @@ void setup() {
   Serial.println(modemInfo);  // not displaying
 
   //radar.debug(MONITOR_SERIAL); //Uncomment to show debug information from the library on the Serial Monitor. By default this does not show sensor reads as they are very frequent.
-  RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
+  //RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
   delay(500);
   Serial.print(F("\nConnect LD2410 radar TX to GPIO:"));
   Serial.println(RADAR_RX_PIN);
   Serial.print(F("Connect LD2410 radar RX to GPIO:"));
   Serial.println(RADAR_TX_PIN);
   Serial.print(F("LD2410 radar sensor initialising: "));
-  if(radar.begin(RADAR_SERIAL))
+  if(radar.begin(sim800))
   {
     Serial.println(F("OK"));
     Serial.print(F("LD2410 firmware version: "));
